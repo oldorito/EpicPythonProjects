5bb0ed99db8290f22fd6f43bc69aa933fc37db5a
oladorito <ola.doroszuk@gmail.com>
2024-02-07 18:54:15 -0500

    silent-auction-bug-fixes
    what the hell was I thinking with the first version

--- silent_auction/silent_auction_main.py
+++ silent_auction/silent_auction_main.py
@@ -9,12 +9,21 @@ other_bidders = True
 def clear_console():
     os.system('cls' if os.name == 'nt' else 'clear')
 
+def highest_bidder(bidding_record):
+    highest_bid = 0
+    winner = ""
+    for bidder in bidding_record:
+        bid_amount = bidding_record[bidder]
+        if bid_amount > highest_bid:
+            highest_bid = bid_amount
+            winner = bidder
+    print(f"The winner is {winner} with a bid of ${highest_bid}")
+
 while other_bidders:
-    bidder_name = input("What is your name?\n")
-    bid_price = input("What is your bid?\n$")
     
-    bids["name"] = bidder_name
-    bids["price"] = bid_price
+    bidder_name = input("What is your name?\n")
+    bid_price = int(input("What is your bid?\n$"))
+    bids[bidder_name] = bid_price
     
     are_there_other_bidders = (input("Are there any other bidders? Type 'yes' or 'no'.\n")).lower()
     if are_there_other_bidders == "yes":
@@ -22,12 +31,5 @@ while other_bidders:
         clear_console()
     else:
         other_bidders = False
-        highest_bid = 0
-        for price in bids:
-            if price > bids[price]:
-                highest_bid = price
-            
-            
-print(f"The winner is {bidder_name} with a bid of ${bid_price}.")
-
-#print(bids)
+        highest_bidder(bids)
+             
