7ea471956ff7e9c9549bde28568873ad5e9fd63a
oladorito <ola.doroszuk@gmail.com>
2024-02-08 08:46:30 -0500

    calculator

--- calculator.py
+++ calculator.py
@@ -1,7 +1,51 @@
-print("Welcone to Ola's EPIC tip calculator! \nIf you don't tip, you're clown.")
-total = float(input("How much was the bill? \n"))
-tip = float(input("How much would you like to tip? 10, 12 or 15 percent? \n"))
-people = float(input("How many people are splitting the bill? \n"))
-final = total / people * (1 + tip / 100)
-final = total/people * (1 + tip/100)
-print(f"Each person should pay {round(final, 2)} dollars.")
+def add(n1, n2):
+    return n1 + n2
+
+def subtract(n1, n2):
+    return n1 - n2
+
+def multiply(n1, n2):
+    return n1 * n2
+
+def divide(n1, n2):
+    if n2 == 0:
+        print("Cannot divide by zero")
+    else:
+        return n1/n2
+
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide,
+}
+
+print("This is Ola's Calculator!\nFor this version, please press ENTER between every number and operation.\nThank you for cooperation")
+
+
+def calculator():
+    num1 = int(input())
+    continue_with_answer = True
+    
+    while continue_with_answer:
+
+        operator = input()
+        if operator not in operations:
+            print("Invalid operator")
+
+        num2 = int(input())
+            
+        answer = operations[operator](num1, num2)
+        print(f"= {answer}")
+    
+        if input("press 'y' to continue on 'n' to restart\n") == "y":
+            num1 = answer
+            print(f"{answer}")
+        else: 
+            continue_with_answer = False
+            calculator()
+
+calculator()
+
+
+
