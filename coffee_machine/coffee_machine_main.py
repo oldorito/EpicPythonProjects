e86714258517b99ef304e53aeb8a3f8f90bc2eb0
oladorito <ola.doroszuk@gmail.com>
2024-02-13 08:59:31 -0500

    coffee-machine-program

---
+++ coffee_machine/coffee_machine_main.py
@@ -0,0 +1,56 @@
+import resources
+
+machine_turned_on = True
+profit = 0
+
+def report():
+    return resources.machine_resources
+
+
+def resource_check(drink_ingredients):
+    for item in drink_ingredients:
+        if drink_ingredients[item] > resources.machine_resources[item]:
+            print(f"Not enough {item}.")
+            return False
+    return True
+
+
+def make_drink(drink, drink_ingredients):
+    for item in drink_ingredients:
+        resources.machine_resources[item] -= drink_ingredients[item]
+    print(f"Here is your {drink}. Enjoy!")
+
+
+def transaction(money_received, drink_cost):
+    global profit
+    if money_received >= drink_cost:
+        change = round(money_received - drink_cost, 2)
+        print(f"Here is your change, ${change}.")
+        profit += drink_cost
+        return True
+    else:
+        print("Not enough money. Money refunded.")
+        return False
+
+
+def coin_processing():
+    print("Insert coins.")
+    total = int(input("quarters: ")) * 0.25
+    total += int(input("dimes: ")) * 0.1
+    total += int(input("nickles: ")) * 0.05
+    total += int(input("pennies: ")) * 0.01
+    return total
+
+
+while machine_turned_on:
+    chosen_drink = input("Choose your coffee: espresso/latte/cappuccino\n").lower()
+    if chosen_drink == "report":
+        print(report())
+    elif chosen_drink == "off":
+        machine_turned_on = False
+    else:
+        drink = resources.MENU[chosen_drink]
+        if resource_check(drink["ingredients"]):
+            payment = coin_processing()
+            if transaction(payment, drink["cost"]):
+                make_drink(chosen_drink, drink["ingredients"])
---
+++ coffee_machine/resources.py
@@ -0,0 +1,33 @@
+from typing import Dict
+
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    }
+}
+
+machine_resources: dict[str, int] = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+}
